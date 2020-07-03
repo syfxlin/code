299c1a7b01514f3127f23a118f6b0be51fce29c2
syfxlin <syfxlin@gmail.com>
2020-07-03 12:16:13 +0800

    :sparkles: feat: Add One Light Pro color scheme

---
+++ out/production/code/Middleware/Middleware2.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class Middleware2 implements MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        echo "Start Middleware2\n";
+        $response = $handler($request);
+        echo "End Middleware2\n";
+        return $response;
+    }
+}
